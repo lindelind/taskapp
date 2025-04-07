b1d974adb2e01e92f143d4adfa1b828f47472e49
Alinde Öst <ost.alinde@gmail.com>
2025-04-07 19:43:44 +0200

    added function to fetch tasks from localstorage and displayed them in taskList. Added a bit of styling.

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,10 +1,46 @@
 import "../style/TaskList.css";
+import React from "react";
+import { useTaskContext } from "../context/TaskContext";
 
 export const TaskList = () => {
+  const { tasks} = useTaskContext();
 
-    return (
-        <>
-        TaskList
-        </>
-    )
-}
+  return (
+    <div className="task-list">
+      <table className="task-table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Beskrivning</th>
+            <th>Förfallodatum</th>
+            <th>Status</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan="5">
+              <hr />
+            </td>
+          </tr>
+          {tasks.map((task) => (
+            <React.Fragment key={task.id}>
+              <tr>
+                <td>{task.title}</td>
+                <td>{task.description}</td>
+                <td>{task.dueDate}</td>
+                <td>{task.done ? "Done" : "Not Done"}</td>
+              </tr>
+                <tr>
+                  <td colSpan="5">
+                    <hr />
+                  </td>
+                </tr>
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
--- client/src/context/TaskContext.jsx
+++ client/src/context/TaskContext.jsx
@@ -1,18 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const TaskContext = createContext();
 
 export const TaskProvider = ({ children }) => {
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const task = JSON.parse(localStorage.getItem(key));
+      if (task) storedTasks.push(task);
+    }
+    setTasks(storedTasks);
+  }, []);
+     
 
   const addTask = (newTask) => {
     localStorage.setItem(newTask.id, JSON.stringify(newTask));
     setTasks((prevTasks) => [...prevTasks, newTask]);
   };
 
- 
+   const fetchTasks = () => {
+     const storedTasks = [];
+     for (let i = 0; i < localStorage.length; i++) {
+       const key = localStorage.key(i);
+       const task = JSON.parse(localStorage.getItem(key));
+       if (task) storedTasks.push(task);
+     }
+     setTasks(storedTasks);
+   };
+
   return (
-    <TaskContext.Provider value={{ tasks, addTask }}>
+    <TaskContext.Provider value={{ tasks, addTask, fetchTasks }}>
       {children}
     </TaskContext.Provider>
   );
