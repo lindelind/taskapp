6673276f2aefc1b1f4697cde681e5a8f2fc64805
Alinde Öst <ost.alinde@gmail.com>
2025-04-09 09:20:31 +0200

    fixed width for columns and the clickable icons. Added a counter for done tasks.

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -6,8 +6,17 @@ export const TaskList = () => {
   const todoTasks = tasks.filter((task) => !task.done);
   const doneTasks = tasks.filter((task) => task.done);
 
+  const totalTasks = tasks.length;
+  const completedTasks = doneTasks.length;
+
   return (
     <div className="task-list">
+    
+      <div>
+        Du har slutfört {""}
+        {completedTasks} av {totalTasks} tasks.
+      </div>
+
       <h2>Att göra</h2>
       <TaskTable
         tasks={todoTasks}
